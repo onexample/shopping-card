579d56ba1b8e0aadf6391d674045764bd6fab835
Vladyslav <onexample@gmail.com>
2016-09-27 15:31:00 +0300

    added core module

---
+++ src/app/core/core.module.js
@@ -0,0 +1,16 @@
+/**
+ * Created by manick on 27.09.16.
+ */
+
+const core=angular.module('app.core',[
+  'ngAnimate',
+  'ngCookies',
+  'ngTouch',
+  'ngSanitize',
+  'ngMessages',
+  'ngAria',
+  'ui.router',
+  'ngMaterial'
+])
+
+export {core};
