20fe72d692dd64cc0ad4b78debb5b18918ce2dac
Vladyslav <onexample@gmail.com>
2016-09-28 15:19:18 +0300

    added toolbar module

--- src/app/toolbar/toolbar.controller.js
+++ src/app/toolbar/toolbar.controller.js
@@ -1,9 +1,45 @@
-class ToolbarController
-{
-  constructor()
-  {
-    'ngInject';
-  }
+
+const _rootScope=new WeakMap();
+const _ShoppingBucketService=new WeakMap();
+
+class ToolbarController {
+
+    constructor(ShoppingBucketService,$rootScope)
+    {
+        'ngInject';
+        this.search='';
+        this.total=0;
+        _ShoppingBucketService.set(this,ShoppingBucketService);
+        _rootScope.set(this,$rootScope);
+        this.activate();
+    }
+
+    activate()
+    {
+        this.getTotal();
+        _rootScope.get(this).$on('itemAdded',()=>{
+            this.getTotal();
+        })
+        _rootScope.get(this).$on('itemRemoved',()=>{
+            this.getTotal();
+        })
+    }
+
+    searchForPhone()
+    {
+        _rootScope.get(this).$emit('searchFired',this.search);
+    }
+
+    clearSearchField()
+    {
+        this.search='';
+    }
+
+    getTotal()
+    {
+        this.total=_ShoppingBucketService.get(this).getItems().length;
+    }
+
 }
 
 export {ToolbarController};
