30f7758acbc3cbf8efc7a04b317dd688a3492192
Vladyslav <onexample@gmail.com>
2016-09-28 12:44:04 +0300

    added notification service

--- src/app/core/core.module.js
+++ src/app/core/core.module.js
@@ -2,15 +2,19 @@
  * Created by manick on 27.09.16.
  */
 
-const core=angular.module('app.core',[
-  'ngAnimate',
-  'ngCookies',
-  'ngTouch',
-  'ngSanitize',
-  'ngMessages',
-  'ngAria',
-  'ui.router',
-  'ngMaterial'
-])
+import {NotificationService} from './services/notification.service.js'
+
+const core=angular
+    .module('app.core',[
+        'ngAnimate',
+        'ngCookies',
+        'ngTouch',
+        'ngSanitize',
+        'ngMessages',
+        'ngAria',
+        'ui.router',
+        'ngMaterial'
+    ])
+    .service('NotificationService',NotificationService);
 
 export {core};
